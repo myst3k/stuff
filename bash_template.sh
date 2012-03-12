01cf70eb35d3feab5307a0e790e9770d0080aa4a
Chris Rushton <myst3k@gmail.com>
2012-03-12 02:26:28 -0400

    updated bash template options

--- bash_template.sh
+++ bash_template.sh
@@ -3,10 +3,9 @@
 # Debug mode, same as set -x
 #set -o xtrace
 
-# set -eu Sames as setting nounset, and errexit
-# Exit if attempting to use uninitialized variable
+# Exit if attempting to use uninitialized variable, same as set -u
 set -o nounset
-# Exit if statement returns non-true value
+# Exit if statement returns non-true value, same as set -e
 set -o errexit
 # Removes the "space" character from the input field separator list
 IFS="$(printf '\n\t')"
